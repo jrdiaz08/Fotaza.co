c49f0a98d7839c2f7bd26cec7a6d78ab7599096b
jrdiaz08 <90425901+jrdiaz08@users.noreply.github.com>
2023-09-13 23:05:34 -0500

    Update captura.js

--- captura.js
+++ captura.js
@@ -1,1 +1,9 @@
-
+cont video = document.getElementById('video');
+  function startup() {
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+}).then(stream => {video.srcObjec = stream;
+                  }).catch(console.error)
+  }
+window.addEventListener('load',startup, false);
