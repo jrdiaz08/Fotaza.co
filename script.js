34e76a59cf03ded384b32d74ca967f6fe733e8d4
jrdiaz08 <90425901+jrdiaz08@users.noreply.github.com>
2023-09-19 23:11:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ const video = document.getElementById("video");
   var ancho = window.innerWidth;
   var alto = window.innerHeight;
   console.log(ancho);
-  console.log(alto);
-  //document.getElementById(' ').style.transform="scale(0)"; // en la seccion "" se altera la propiedad css escala 
-var anchovideo = ancho;
-var altovideo = ancho*1.33;    
+  console.log(alto); 
+  var anchovideo = ancho;
+  var altovideo = ancho*1.33;
+  var altorestante = alto-altovideo;
+  var altocomandos = altorestante*0.8;
+  var altofinal = altorestante*0.2;
     
+  document.getElementById('fondo').style.width=ancho; // en la seccion "fondo" se altera la propiedad css width 
+  document.getElementById('fondo').style.heigth=alto; // en la seccion "fondo" se altera la propiedad css heigth
+    
+  document.getElementById('pantalla').style.width=ancho; // en la seccion "pantalla" se altera la propiedad css width 
+  document.getElementById('pantalla').style.heigth=altovideo; // en la seccion "pantalla" se altera la propiedad css heigth
+  
+  document.getElementById('comandos').style.width=ancho; // en la seccion "comandos" se altera la propiedad css width 
+  document.getElementById('comandos').style.heigth=altocomandos; // en la seccion "<comandos" se altera la propiedad css heigth
+
+  document.getElementById('final').style.width=ancho; // en la seccion "final" se altera la propiedad css width 
+  document.getElementById('final').style.heigth=altofinal; // en la seccion "final" se altera la propiedad css heigth
+
   navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: { width: AnchoVideo, height: AltoVideo },
+    video: { width: anchovideo, height: altovideo },
 }).then(stream => {video.srcObject = stream;
                   }).catch(console.error)
   }
