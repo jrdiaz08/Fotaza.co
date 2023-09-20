9769203dc2eba8e4dd29c24d5e449ded2faed3ec
jrdiaz08 <90425901+jrdiaz08@users.noreply.github.com>
2023-09-19 22:50:19 -0500

    Delete captura.js

--- captura.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById("video");
-  function startup() {
-  navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: { width: AnchoVideo, height: AltoVideo },
-}).then(stream => {video.srcObject = stream;
-                  }).catch(console.error)
-  }
-window.addEventListener('load',startup, false);
