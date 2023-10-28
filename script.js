61a9b764807ef1977e14f2dba16c902e61d7cf78
jrdiaz08 <90425901+jrdiaz08@users.noreply.github.com>
2023-10-28 08:52:31 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function modo() {
     cambioCamara = "environment";
     console.log("Camara= ",cambioCamara);
     videoconfig = {audio: false,
-      video: { facingMode: cambioCamara, width: ancho, height: altovideo }
+      video: { facingMode: cambioCamara }
     };  navigator.mediaDevices.getUserMedia(videoconfig).then(stream => { 
       video.srcObject = stream,
       canales = stream.getTracks();
