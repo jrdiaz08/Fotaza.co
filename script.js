825321981ad15bb087f07678ea4b30dc870b000c
unknown <jose.diaz.mercado@gmail.com>
2023-12-25 01:35:41 -0500

    smoothing 3

--- script.js
+++ script.js
@@ -123,9 +123,9 @@ function capturar() {
   ubicacion.insertBefore(lienzo, referencia);
   var contexto = lienzo.getContext('2d');
   contexto.imageSmoothingEnabled = false;
-  contexto.drawImage(video, 0, 0, parseInt(ancho), parseInt(altopantalla));
+  contexto.drawImage(video, 0, 0, ancho, altopantalla);
   contexto.imageSmoothingEnabled = true;
-  contexto.drawImage(vortice, centro, 0, parseInt(vortice.width), parseInt(vortice.height));
+  contexto.drawImage(vortice, centro, 0, (vortice.width), (vortice.height));
   document.getElementById('modo').style.display = "none"; // en la seccion "modo" se altera la propiedad css display
   document.getElementById('captura').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
   document.getElementById('limpiar').style.display = "inline-block"; // en la seccion "limpiar" se altera la propiedad css display
