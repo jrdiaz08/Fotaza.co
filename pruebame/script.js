3adc4128a0c2303fbf08fd4c320604abdce5d5b8
jrdiaz08 <90425901+jrdiaz08@users.noreply.github.com>
2024-02-12 01:06:35 -0500

    Add files via upload

---
+++ pruebame/script.js
@@ -0,0 +1,183 @@
+let video = document.getElementById("video");
+let titular = document.getElementById("titular");
+let canales;
+let cambioCamara = "user";
+var videoconfig = {audio: false,
+  video: {facingMode: cambioCamara, autoPlay: true, playsinline: true,
+ muted: true}
+};
+var alto = window.innerHeight; 
+var ancho = window.innerWidth;
+var altocomandos = alto*0.1;
+var altofinal = alto*0.1;
+var altopantalla = ancho*1.33;
+var altovideo = ancho*1.33;
+var altosuperior = (alto-altopantalla-altocomandos-altofinal)/2;
+var altoinferior = (alto-altopantalla-altocomandos-altofinal)/2;
+var total = altosuperior+altopantalla+altoinferior+altocomandos+altofinal;
+
+var lienzo = document.createElement("canvas");
+  lienzo.id = "lienzo";
+  lienzo.width = ancho; 
+  lienzo.height = altovideo;
+  lienzo.style.position="absolute";
+  lienzo.style.top =altosuperior + "px";
+  lienzo.style.left="0%";
+  lienzo.style.zIndex="2";
+
+function startup() {
+  navigator.mediaDevices.getUserMedia(videoconfig).then(stream => { 
+    video.srcObject = stream,
+    canales = stream.getTracks();
+    ;
+  }).catch(console.error)
+
+  console.log("alto= ",alto);
+  console.log("ancho= ",ancho);
+  console.log("alto superior= ",altosuperior);
+  console.log("alto pantalla= ",altopantalla);
+  console.log("alto inferior= ",altoinferior);
+  console.log("alto comandos= ",altocomandos);
+  console.log("alto final= ",altofinal);
+  console.log("alto total= ",total);
+  console.log("alto video= ",(video.height));
+  console.log("ancho video= ",(video.width));
+
+  document.getElementById('limpiar').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
+  document.getElementById('guardar').style.display = "none"; // en la seccion "guardar" se altera la propiedad css display
+
+  document.getElementById('fondo').style.height = alto + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('fondo').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width
+  document.getElementById('superior').style.height = altosuperior + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('superior').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width  
+  document.getElementById('pantalla').style.height = altopantalla + "px"; // en la seccion "pantalla" se altera la propiedad css heigth
+  document.getElementById('pantalla').style.width = ancho + "px"; // en la seccion "pantalla" se altera la propiedad css width 
+  document.getElementById('video').style.height = altovideo + "px"; // en la seccion "pantalla" se altera la propiedad css height
+  document.getElementById('video').style.width = ancho + "px"; // en la seccion "pantalla" se altera la propiedad css width
+
+  document.getElementById('inferior').style.height = altoinferior + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('inferior').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width 
+  document.getElementById('comandos').style.height = altocomandos + "px"; // en la seccion "comandos" se altera la propiedad css heigth
+  document.getElementById('comandos').style.width = ancho + "px"; // en la seccion "comandos" se altera la propiedad css width 
+  document.getElementById('final').style.height = altofinal + "px"; // en la seccion "final" se altera la propiedad css heigth
+  document.getElementById('final').style.width = ancho + "px"; // en la seccion "final" se altera la propiedad css width 
+  
+  document.getElementById('titular').style.top = altosuperior + "px"; // en la seccion "final" se altera la propiedad css heigth
+  document.getElementById('titular').style.left = 0 + "px"; // en la seccion "final" se altera la propiedad css width 
+
+}
+  
+  window.addEventListener('load',startup, false);
+
+function fecha() { // funcion que extrae la fecha del navegador, codigo descargado
+  var hoy = new Date();
+  var m = new Array();
+  var d = new Array();
+  var an= hoy.getFullYear();
+  m[0]="Enero";  m[1]="Febrero";  m[2]="Marzo";
+  m[3]="Abril";   m[4]="Mayo";  m[5]="Junio";
+  m[6]="Julio";    m[7]="Agosto";   m[8]="Septiembre";
+  m[9]="Octubre";   m[10]="Noviembre"; m[11]="Diciembre";
+  document.write(hoy.getDate());
+  document.write(" de ");
+  document.write(m[hoy.getMonth()]);
+  document.write(" ");
+  document.write(hoy.getFullYear());   
+  }
+
+function modo() {
+  document.getElementById('modo').style.transform = "scaleX(-0.8)scaleY(0.8)";
+  if(cambioCamara == "user"){
+   canales.forEach(track => track.stop())
+    cambioCamara = "environment";
+    console.log("Camara= ",cambioCamara);
+    videoconfig = {audio: false,
+      video: {facingMode: cambioCamara, autoPlay: true,
+playsInline: true, muted: true}
+    };  navigator.mediaDevices.getUserMedia(videoconfig).then(stream => { 
+      video.srcObject = stream,
+      canales = stream.getTracks();
+      ;
+    }).catch(console.error)
+  }
+  else{
+    canales.forEach(track => track.stop())
+    cambioCamara = "user";
+    console.log("Camara= ",cambioCamara);
+    videoconfig = {audio: false,
+      video: {facingMode: cambioCamara, autoPlay: true,
+      playsInline: true, muted: true}
+    }; navigator.mediaDevices.getUserMedia(videoconfig).then(stream => { 
+      video.srcObject = stream,
+      canales = stream.getTracks();
+      ;
+    }).catch(console.error)
+  }
+  setTimeout(pausamodo, 100); 
+};
+function pausamodo(){
+  document.getElementById('modo').style.transform = "scaleX(1)scaleY(1)";
+}
+
+function capturar() {
+  document.getElementById('capturar').style.transform = "scaleX(-0.8)scaleY(0.8)";
+  var ubicacion = document.getElementById("pantalla");
+  const referencia = document.getElementById("titular");
+  ubicacion.insertBefore(lienzo, referencia);
+  var contexto = lienzo.getContext('2d');
+
+  const densidadpixeles = window.devicePixelRatio;
+  const posicion = lienzo.getBoundingClientRect();
+  lienzo.width = posicion.width *densidadpixeles;
+  lienzo.height = posicion.height *densidadpixeles;
+  contexto.scale(densidadpixeles, densidadpixeles);
+  lienzo.style.width = `${posicion.width}px`;
+  lienzo.style.height = `${posicion.height}px`;
+  
+  contexto.imageSmoothingEnabled = true;
+  contexto.drawImage(video, 0, 0, parseInt(ancho), parseInt(altovideo));
+ // contexto.imageSmoothingEnabled = true;
+  contexto.drawImage(titular, 0, 0, (titular.width), (titular.height));
+  setTimeout(pausacapturar, 100); 
+};
+function pausacapturar(){
+  document.getElementById('capturar').style.transform = "scaleX(1)scaleY(1)";
+
+  document.getElementById('modo').style.display = "none"; // en la seccion "modo" se altera la propiedad css display
+  document.getElementById('capturar').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
+  document.getElementById('limpiar').style.display = "inline-block"; // en la seccion "limpiar" se altera la propiedad css display
+  document.getElementById('guardar').style.display = "inline-block"; // en la seccion "guardar" se altera la propiedad css display
+};
+
+function limpiar() { 
+ document.getElementById('limpiar').style.transform = "scaleX(-0.8)scaleY(0.8)"; 
+ setTimeout(pausalimpiar, 100); 
+};
+function pausalimpiar(){
+ document.getElementById('limpiar').style.transform = "scaleX(1)scaleY(1)";
+ lienzo.width=lienzo.width;
+ document.getElementById('limpiar').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
+ document.getElementById('guardar').style.display = "none"; // en la seccion "guardar" se altera la propiedad css display
+ document.getElementById('modo').style.display = "inline-block"; // en la seccion "modo" se altera la propiedad css display
+ document.getElementById('capturar').style.display = "inline-block"; // en la seccion "capturar" se altera la propiedad css display
+};
+
+function guardar() { 
+  document.getElementById('guardar').style.transform = "scaleX(-0.8)scaleY(0.8)"; 
+  let enlace = document.createElement('a');
+      // El título
+      enlace.download = Date.now();
+      // Convertir la imagen a Base64 y ponerlo en el enlace
+      enlace.href = lienzo.toDataURL();
+      // Hacer click en él
+      enlace.click();
+    setTimeout(pausaguardar, 100); 
+};
+function pausaguardar(){
+  document.getElementById('guardar').style.transform = "scaleX(1)scaleY(1)";
+  lienzo.width=lienzo.width;
+  document.getElementById('limpiar').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
+  document.getElementById('guardar').style.display = "none"; // en la seccion "guardar" se altera la propiedad css display
+  document.getElementById('modo').style.display = "inline-block"; // en la seccion "modo" se altera la propiedad css display
+  document.getElementById('capturar').style.display = "inline-block"; // en la seccion "capturar" se altera la propiedad css display
+ };
--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function fecha() { // funcion que extrae la fecha del navegador, codigo descarga
   document.write(m[hoy.getMonth()]);
   document.write(" ");
   document.write(hoy.getFullYear());   
-  }
+  };
 
 document.body.onload = function inicio() {
   var alto = window.innerHeight; 
@@ -78,8 +78,77 @@ setTimeout(function () { // se ejecuta la funcion una vez se carga la pagina, co
    // en la seccion loader se altera la propiedad css escala   
  },2500); // el retraso definido en milisegundos
 
-}
+ };
  
+ let video = document.getElementById("video");
+ let titular = document.getElementById("titular");
+ let canales;
+ let cambioCamara = "user";
+ var videoconfig = {audio: false,
+   video: {facingMode: cambioCamara, autoPlay: true, playsinline: true,
+  muted: true}
+};
+
+var alto = window.innerHeight; 
+var ancho = window.innerWidth;
+var altocomandos = alto*0.1;
+var altofinal = alto*0.1;
+var altopantalla = ancho*1.33;
+var altovideo = ancho*1.33;
+var altosuperior = (alto-altopantalla-altocomandos-altofinal)/2;
+var altoinferior = (alto-altopantalla-altocomandos-altofinal)/2;
+var total = altosuperior+altopantalla+altoinferior+altocomandos+altofinal;
+
+var lienzo = document.createElement("canvas");
+  lienzo.id = "lienzo";
+  lienzo.width = ancho; 
+  lienzo.height = altovideo;
+  lienzo.style.position="absolute";
+  lienzo.style.top =altosuperior + "px";
+  lienzo.style.left="0%";
+  lienzo.style.zIndex="2";
+
+
+  navigator.mediaDevices.getUserMedia(videoconfig).then(stream => { 
+    video.srcObject = stream,
+    canales = stream.getTracks();
+    ;
+  }).catch(console.error)
+
+  console.log("alto= ",alto);
+  console.log("ancho= ",ancho);
+  console.log("alto superior= ",altosuperior);
+  console.log("alto pantalla= ",altopantalla);
+  console.log("alto inferior= ",altoinferior);
+  console.log("alto comandos= ",altocomandos);
+  console.log("alto final= ",altofinal);
+  console.log("alto total= ",total);
+  console.log("alto video= ",(video.height));
+  console.log("ancho video= ",(video.width));
+
+  document.getElementById('limpiar').style.display = "none"; // en la seccion "limpiar" se altera la propiedad css display
+  document.getElementById('guardar').style.display = "none"; // en la seccion "guardar" se altera la propiedad css display
+
+  document.getElementById('fondo').style.height = alto + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('fondo').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width
+  document.getElementById('superior').style.height = altosuperior + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('superior').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width  
+  document.getElementById('pantalla').style.height = altopantalla + "px"; // en la seccion "pantalla" se altera la propiedad css heigth
+  document.getElementById('pantalla').style.width = ancho + "px"; // en la seccion "pantalla" se altera la propiedad css width 
+  document.getElementById('video').style.height = altovideo + "px"; // en la seccion "pantalla" se altera la propiedad css height
+  document.getElementById('video').style.width = ancho + "px"; // en la seccion "pantalla" se altera la propiedad css width
+
+  document.getElementById('inferior').style.height = altoinferior + "px"; // en la seccion "fondo" se altera la propiedad css heigth
+  document.getElementById('inferior').style.width = ancho + "px"; // en la seccion "fondo" se altera la propiedad css width 
+  document.getElementById('comandos').style.height = altocomandos + "px"; // en la seccion "comandos" se altera la propiedad css heigth
+  document.getElementById('comandos').style.width = ancho + "px"; // en la seccion "comandos" se altera la propiedad css width 
+  document.getElementById('final').style.height = altofinal + "px"; // en la seccion "final" se altera la propiedad css heigth
+  document.getElementById('final').style.width = ancho + "px"; // en la seccion "final" se altera la propiedad css width 
+  
+  document.getElementById('titular').style.top = altosuperior + "px"; // en la seccion "final" se altera la propiedad css heigth
+  document.getElementById('titular').style.left = 0 + "px"; // en la seccion "final" se altera la propiedad css width 
+
+  
 window.addEventListener('resize', function(event){
   var alto = window.innerHeight; 
   var ancho = window.innerWidth;
